07244cd95d53344e768e6d37f33e11918d0569d2
ibrahimvau5518 <jibonm676@gmail.com>
2026-05-24 03:31:55 -0400

    add root route for vercel

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/services', require('./routes/serviceRoutes'));
