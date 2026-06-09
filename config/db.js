c62a9d9aa2494e08af094ab96cdc244caeef38e6
ibrahimvau5518 <jibonm676@gmail.com>
2026-06-09 02:42:29 -0400

    Fix MongoDB connection error by removing deprecated options

--- config/db.js
+++ config/db.js
@@ -9,10 +9,7 @@ const connectDB = async () => {
     }
 
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         isConnected = conn.connection.readyState === 1;
         console.log(`MongoDB Connected Successfully: ${conn.connection.host}`);
     } catch (err) {
